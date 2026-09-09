f742dfc9c035b9b7f4fe6618fd2cafc7544fbd87
GioR79 <97035475+GioR79@users.noreply.github.com>
2026-09-09 16:44:13 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,8 +12,20 @@ self.addEventListener("install", e =>
   )
 );
 
-self.addEventListener("fetch", e =>
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  )
-);
+self.addEventListener("fetch", e => {
+  if (e.request.mode === "navigate") {
+    e.respondWith(
+      fetch(e.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put("./index.html", copy));
+          return response;
+        })
+        .catch(() => caches.match("./index.html"))
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(r => r || fetch(e.request))
+    );
+  }
+});
